90266dc8f203759afe3e73b434aec9eadcfbee4e
MozartCuidadorDigital <45375029+nandostiwar@users.noreply.github.com>
2024-07-03 18:36:09 -0500

    se integro base datos MongoDB

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,40 @@
+const pool = require('../database/mongo')
 const axios = require('axios');
+const CryptoJS = require("crypto-js");
+const moment = require('moment-timezone');
 
 const getMozart = (req, res) => {
   res.send('API para Mozart');
 };
 
+
+//---------------Login---------------------
 const postLogin = async (req, res) => {
   const datos = req.body;
-  console.log("LOGIN: ", datos);
-
-  let user = "feliperodriguezjar@gmail.com";
-  let pass = "admin";
-
-  if (datos.email === user && datos.password === pass) {
-    res.json({ status: "Bienvenido", user: datos.email, role: "admin"});
-  } else {
-    res.json({ status: "Error" });
+  //console.log("LOGIN: ", datos);
+  const hashedPassword = CryptoJS.SHA256(datos.password, process.env.CODE_SECRET_DATA).toString();
+  //console.log("PASSS: ", hashedPassword);
+  try{
+    const users =  await pool.db('pocketux').collection('users').find().toArray()
+    console.log("USERS: ", users);
+    const login =  await pool.db('pocketux').collection('users').findOne({ email: datos.email, password: hashedPassword });
+    if (login) {
+      // Obtener la fecha y hora actual en formato Bogotá
+      const currentDateTime = moment().tz('America/Bogota').format('YYYY-MM-DD HH:mm:ss');
+      // Almacenar en la colección log_login
+      await pool.db('pocketux').collection('log_login').insertOne({ email: datos.email, role: login.role, date: currentDateTime });
+      res.json({ status: "Bienvenido", user: datos.email, role: login.role});
+    } else {
+      res.json({ status: "ErrorCredenciales" });
+    }
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    res.status(500).json({ status: "Error", message: "Internal Server Error" });
   }
 };
 
 
-
+//---------------API para Mozart---------------------
 const postMozart = async (req, res) => {
   const datos = req.body;
 
@@ -52,12 +67,24 @@ const postMozart = async (req, res) => {
   }
 };
 
+
+//---------------API para envio DEMO a MAKE---------------------
 const postDemo = async (req, res) => {
   const datos = req.body;    
-  console.log("DATOS: ", datos)
+
+  // Obtener la fecha y hora actual en formato Bogotá
+  const currentDateTime = moment().tz('America/Bogota').format('YYYY-MM-DD HH:mm:ss');
+  
+  // Crear el objeto con la fecha y hora incluida
+  const dataToSend = {
+    date: currentDateTime,
+    ...datos    
+  };
+
+  console.log("DATOS ENVIAR: ", dataToSend)
 
   const axios_url = "https://hook.us1.make.com/3uh3nnblj1j2ffrtyph9k9ldt19y03x9";
-  const dataToSend = datos.body;
+  //const dataToSend = datos.body;
 
   const config = {
     headers: {
@@ -69,6 +96,7 @@ const postDemo = async (req, res) => {
   try {
     const response = await axios.post(axios_url, dataToSend, config);
     console.log('Respuesta del servidor:', response.data);
+    await pool.db('pocketux').collection('log_demo').insertOne(dataToSend);
     res.send(response.data); // Enviar la respuesta de vuelta al cliente
   } catch (error) {
     console.error('Error al enviar la solicitud:', error);
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const cors = require('cors');
+const pool = require('./database/mongo');
+require('dotenv').config();
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.use(cors()); // Permitir todos los orígenes
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Bienvenido a PocketUX!');
-  });
+//connectToDatabase();
+
+/* app.get('/', async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const usersCollection = db.collection('users');
+    const users = await usersCollection.find().toArray();
+    console.log('Fetched users from MongoDB:', users);
+    res.send(users);
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    res.status(500).send('Failed to fetch users from MongoDB');
+  }
+}); */
 
 const userRoutes = require('./routes/userRoutes');
 app.use('/api', userRoutes);
@@ -16,3 +29,5 @@ app.use('/api', userRoutes);
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+
